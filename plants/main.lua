68439c778ae80f249eec7c70a1da1a47a2756c51
xarvh <francesco.orsenigo@gmail.com>
2020-02-08 23:05:51 +0100

    wip

--- plants/main.lua
+++ plants/main.lua
@@ -1,31 +1,8 @@
 local pprint = require('pprint')
 
 
-
-
---[[
-for any ellipse in listOfEllipses
-  if within range
-    color = colorTexture in ellipsesCoordinates
-    alpha = alphaTexture in ellipsesCoordinates
-
-    colorSum += alpha * color
-    totalAlpha += alpha
-
-color = colorSum / totalAlpha
-]]
-
-
-
-
-
-
-
 local maxBranchesPerTree = 10
 local maxChildrenPerBranch = 3
-
-
-
 
 
 
@@ -46,125 +23,8 @@ vec4 position( mat4 transform_projection, vec4 vertex_position )
 
 
 
-local plantFragmentShader = [[
-/*
- * This is a GLSL implementation of http://www.jeffreythompson.org/collision-detection/poly-point.php
- */
-
-
-
-
-
-bool pointProjectionIntersectsSegment(vec2 a, vec2 b, vec2 p) {
-
-    // if py does not lie between ay and by, we're out
-    if ((a.y > p.y) == (b.y > p.y)) {
-        return false;
-    }
-
-    /* This looks eerily similar to the equation of a line by two points, but I'm not sure why it works here.
-
-       *MAGIC!*
-
-       p.x - a.x   b.x - a.x
-       --------- < ---------
-       p.y - a.y   b.y - a.y
-
-
-       Weirdly if I move the (b.y - a.y) to the left to get replace the division with a multiplication
-       it behaves weirdly, whether I adjust the comparison op or not.
-       I assume that this is related to the sign of (b.y - a.y).
-    */
-
-    return (p.x - a.x) < (b.x - a.x) * (p.y - a.y) / (b.y - a.y);
-}
-
-
-
-bool pointInsideQuad(vec2 p, vec2 a, vec2 b, vec2 c, vec2 d) {
-
-    bool collision = false;
-
-    if (pointProjectionIntersectsSegment(a, b, p)) collision = !collision;
-    if (pointProjectionIntersectsSegment(b, c, p)) collision = !collision;
-    if (pointProjectionIntersectsSegment(c, d, p)) collision = !collision;
-    if (pointProjectionIntersectsSegment(d, a, p)) collision = !collision;
-
-    return collision;
-}
-
-
-
-/*
- * LOVE stuff
- */
-#define branches_per_tree ]] .. maxBranchesPerTree .. "\n" .. [[
-#define vertex_per_branch 4
-#define leaves_per_tree branches_per_tree
-
-varying vec2 v_pos;
-uniform vec2 u_size;
-uniform vec2 u_topLeft;
-uniform vec2 u_branches[branches_per_tree * vertex_per_branch];
-uniform vec4 u_leaves[leaves_per_tree];
-
-uniform Image u_alphaBrush;
-uniform Image u_colorBrush;
-
-vec4 effect(vec4 _, Image __, vec2 ___, vec2 ____ ) {
-
-    bool isInsideBranch = false;
-    vec2 ub[] = u_branches;
-    for (int b = 0; b < branches_per_tree * vertex_per_branch; b += vertex_per_branch) {
-      if (pointInsideQuad(v_pos, ub[b], ub[b + 1], ub[b + 2], ub[b + 3])) {
-        isInsideBranch = true;
-        break;
-      }
-    }
-
-    vec4 branchColor = vec4(0.48, 0.31, 0.2, 1.0);
-
-    vec3 leavesColorAccum = vec3(0, 0, 0);
-    float leavesAlphaAccum = 0;
-    int leavesCount = 0;
-    for (int l = 1; l < leaves_per_tree; l++) {
-      vec4 leaf = u_leaves[l];
-      float ww = leaf.z;
-      float hh = leaf.w;
-      vec2 dp = v_pos - leaf.xy;
-
-      if (dp.x * dp.x * hh + dp.y * dp.y * ww <= ww * hh) {
-
-        vec3 color = Texel(u_colorBrush, dp + 0.5).r * vec3(0.2, 1.0, 0.1) * 0.6 + vec3(0.1, 0.4, 0.0);
-        float alpha = Texel(u_alphaBrush, dp + 0.5).r > 0.3 ? 1.0 : 0.0;
-        if (alpha > 0.0) {
-          leavesColorAccum += color * alpha;
-          leavesAlphaAccum += alpha;
-          leavesCount += 1;
-        }
-      }
-    }
-
-
-    vec4 leavesColor = vec4(leavesColorAccum / leavesAlphaAccum, leavesAlphaAccum / leavesCount);
-
-    if (leavesCount == 0) {
-      if (isInsideBranch) {
-        return branchColor;
-      } else {
-        discard;
-      }
-    }
-
-    if (isInsideBranch) {
-      return mix(branchColor, leavesColor, leavesAlphaAccum / leavesCount);
-    }
-
-    return leavesColor;
-}
-
-
-]]
+local plantFragmentShader =
+    love.filesystem.read('plant.frag'):gsub('@@maxBranchesPerTree@@', maxBranchesPerTree)
 
 
 -- Linear --------------------------------------------------------------------
@@ -379,7 +239,7 @@ function treeGetLeaves(tree)
     for i, b in ipairs(tree) do
       local w = math.max(b.bottomWidth, b.length)
       local h = 2 * math.min(b.bottomWidth, b.length)
-      table.insert(leaves, { b.tip.x, b.tip.y, w * w, h * h })
+      table.insert(leaves, { b.tip.x, b.tip.y, w, h })
     end
     return leaves
 end
